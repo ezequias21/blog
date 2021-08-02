5a713ed742f2269b0fd57bc5b2c06625e4788e4f
ezequias <ezequiasantunes18@gmail.com>
2021-08-01 21:17:42 -0300

    Segundo commit

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ include  APP . '/Helpers/Url.php';
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso de PHP7 e MVC</title>
+    <title>Curso de PHP7</title>
 </head>
 <body>
     <?php
